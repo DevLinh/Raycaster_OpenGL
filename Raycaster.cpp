0072df4afbcd2d728aee0b3c7b2efbc130475156
devlinh <devlinh99qb@gmail,com>
2020-08-16 16:36:50 +0700

    tao thanh dieu huong va phat trien chuc nang di chuyen cho player

--- Raycaster.cpp
+++ Raycaster.cpp
@@ -2,8 +2,12 @@
 #include "Dependencies/glew/glew.h"
 #include "Dependencies/freeglut/freeglut.h"
 #include <math.h>
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 
-float px, py; // vị trí của player
+float px, py, pdx, pdy, pa;
+//px, py vị trí của player
+//pdx, pdy là delta x, delta y, một khoảng lệch với vị trí player
+//pa là góc nhìn của player - player angle, ở đây mang giá trị radian
 
 int mapX = 8, mapY = 8, mapS = 64; // bản đồ là một lưới vuông kích thước 8x8, mỗi ô có chiều dài cạnh là 64
 // ta lưu trữ thông tin của bản đồ trong 1 mảng 1 chiều 64 phần tử, giá trị 1 đại diện cho tường, chướng ngại vật
@@ -65,6 +69,15 @@ void drawPlayer() {
 	glBegin(GL_POINTS);
 	glVertex2i(px, py);
 	glEnd();
+
+	// vẽ thanh điều hướng khi có pa (khi nhấm A hoặc D)
+	glLineWidth(3); // thanh điều hướng màu vang
+	glBegin(GL_LINES);
+	//vị trí player chính là điểm đầu của thanh điều hướng
+	glVertex2i(px, py);
+	// điểm còn lại được xác định theo góc pa, truy nhiên pdx và pdx các player 1 khoảng nhó, ta nhân đồng thời với 10 để tăng chiều dài của thanh điều hướng
+	glVertex2i(px + pdx * 5, py + pdy * 5);
+	glEnd();
 }
 
 void display()
@@ -81,10 +94,40 @@ void display()
 void buttons(unsigned char key, int x, int y)
 {
 	// di chuyển vị trí 1 khoảng 5 đơn vị tùy hướng di chuyển
+	/*
 	if (key == 'a') { px -= 5; }; // 'a' thì di chuyển qua trái, vị trí bị dịch chuyển giảm trên trục hoành 1 khoảng 5 đơn vị
 	if (key == 'd') { px += 5; }; // ngược lại với 'a'
 	if (key == 'w') { py -= 5; } // 'w' thì di chuyển lên, vị trí dịch chuyển lại gần về 0 trên trục tung, vị trí bị dịch chuyển 1 khoảng 5 đơn vị trên trục tung
 	if (key == 's') { py += 5; } // ngược lại với 'w'
+	*/
+	// Ta cần player di chuyển linh hoạt hơn, player cần có thể xoay 360 độ và di chuyển theo hướng đó
+	//Hai phim A và D giờ đóng vai trò thay đổi hướng di chuyển của player, sẽ được biểu diễn bởi một thanh điều hướng ngắn
+	if (key == 'a') 
+	{ 
+		pa -= 0.1;  // A hướng sang trái, đồng nghĩa việc xoay ngược chiều kim đồng hồ
+		// đưa pa về 2PI khi pa < 0
+		if (pa < 0) 
+		{ 
+			pa += 2 * PI; //360 độ tương đương 2*PI
+		} 
+		// tính tọa độ đỉnh của thanh điều hướng khi biết góc thông qua HỆ THỨC LƯỢNG TRONG TAM GIÁC VUÔNG
+		// giá trị 5 ở công thức dưới chính là khoảng cách giữa vị trí player so với điểm cuối ( đầu còn lại của thanh điều hướng )
+		pdx = cos(pa) * 5; 
+		pdy = sin(pa) * 5; 
+	};
+	if (key == 'd') 
+	{ 
+		pa += 0.1; 
+		if (pa > 2 * PI) 
+		{ 
+			pa -= 2 * PI; 
+		} 
+		pdx = cos(pa) * 5; 
+		pdy = sin(pa) * 5; 
+	};
+	// Sau khi xác định được hướng, W và S đơn giản thay đổi vị trí tiến hoặc lùi của player
+	if (key == 'w') { px += pdx; py += pdy; };
+	if (key == 's') { px -= pdx; py -= pdy; };
 	glutPostRedisplay(); // tiến hành vẽ lại trên màn hình mỗi khi nhấn phím
 }
 
