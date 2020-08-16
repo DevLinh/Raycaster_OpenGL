6894a8655d4ef5e37480bff34c0e6f978dee87e6
devlinh <devlinh99qb@gmail,com>
2020-08-16 15:01:47 +0700

    Add project files.

---
+++ Raycaster.cpp
@@ -0,0 +1,32 @@
+﻿// Raycaster_OpenGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include "Dependencies/glew/glew.h"
+#include "Dependencies/freeglut/freeglut.h"
+
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void init()
+{
+	//set màu nền về màu đen xám
+	glClearColor(0.3, 0.3, 0.3, 0);
+	//set window dimension 1024 x 512 pixels
+	gluOrtho2D(0, 1024, 512, 0);
+};
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv); //Initialize GLUT
+	glutInitDisplayMode(GLUT_SINGLE || GLUT_RGBA); //Enable double bufferd mode
+	glutInitWindowSize(1024, 512); //Khởi tạo cửa sổ 1024x512
+	glutInitWindowPosition(100, 100); // vị trí cửa sổ khởi đầu 100x100
+	glutCreateWindow("Raycaster");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
+
